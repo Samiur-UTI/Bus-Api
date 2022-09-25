0202b3ef4bb46f95307c7077053edcfb7db7104b
SAMIUR <samiurkhan94@gmail.com>
2022-09-26 00:46:15 +0600

    route edit

---
+++ module/model/route.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    "route",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      from: {
+        type: DataTypes.STRING,
+        validate:{
+            notEmpty: true
+        }
+      },
+      to: {
+        type: DataTypes.STRING,
+        validate:{
+          notEmpty: true
+      }
+      },
+      status:{
+        type:DataTypes.ENUM({
+            values: ['ACTIVE', 'INACTIVE']
+          })
+      }
+    },
+    {
+      tableName: "route",
+      freezeTableName: true,
+      timestamps: false,
+      underscored: true,
+    }
+  );
+};
